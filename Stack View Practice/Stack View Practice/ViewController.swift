4632eb6203cac23d291a0d6d394b6d25ab413225
Catherine Chen <yacatherine19@gmail.com>
2018-10-11 23:18:53 +0800

    [iOS] stack view

---
+++ Stack View Practice/Stack View Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stack View Practice
+//
+//  Created by Catherine Chen on 10/10/2018.
+//  Copyright © 2018 Catherine Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
