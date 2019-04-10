8b6c1d502b4879e81bf0dd0877134dda6c7a82c9
Catherine Chen <catherine_chen@trendmicro.com>
2019-04-10 17:14:17 +0800

    [iOS] initialised SDK and sample app

---
+++ SDK/MySDK/MySDK/MySDK.h
@@ -0,0 +1,19 @@
+//
+//  MySDK.h
+//  MySDK
+//
+//  Created by Catherine Chen (RD-TW) on 10/04/2019.
+//  Copyright © 2019 Catherine Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySDK.
+FOUNDATION_EXPORT double MySDKVersionNumber;
+
+//! Project version string for MySDK.
+FOUNDATION_EXPORT const unsigned char MySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySDK/PublicHeader.h>
+
+
---
+++ SDK/SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Catherine Chen (RD-TW) on 10/04/2019.
+//  Copyright © 2019 Catherine Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
